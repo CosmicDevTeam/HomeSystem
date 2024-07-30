fe5abebd7442759d54804db91b2db0ec84530235
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-07-29 19:38:35 -0700

    Actualizar HomeSystem.php

--- src/homes/zephy/HomeSystem.php
+++ src/homes/zephy/HomeSystem.php
@@ -1,1 +1,23 @@
-A
+<?php
+
+namespace zephy\homes;
+
+use muqsit\invmenu\InvMenuHandler;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+use zephy\homes\commands\HomeCommand;
+
+class HomeSystem extends PluginBase {
+    use SingletonTrait;
+    const PREFIX = "§6(§gHomeSystem§6) §r";
+
+    protected function onEnable(): void
+    {
+        self::setInstance($this);
+        $this->getServer()->getCommandMap()->register("HomeSystem", new HomeCommand());
+        if(!InvMenuHandler::isRegistered()){
+            InvMenuHandler::register($this);
+        }
+    }
+
+}

aa535ce457eb43e1c06539b9083b36c0ee3bf002
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-03 22:59:32 -0700

    Ordenado

--- src/zephy/homes/data/HomeFactory.php
+++ src/zephy/homes/data/HomeFactory.php
@@ -10,13 +10,17 @@ use zephy\homes\util\Utils;
 
 class HomeFactory {
     use SingletonTrait;
+    
     private array $homes = [];
+    
     public function existsPlayer(Player $player): bool {
         return isset($this->homes[$player->getName()]);
     } 
+    
     public function getHomes(Player $player): array {
         return $this->homes[$player->getName()];
     }
+    
     public function getHome(string|Player $player, string $home): ?Home{
         if($player instanceof Player) {
             if (!isset($this->homes[$player->getName()])) return null;
@@ -26,25 +30,29 @@ class HomeFactory {
             return $this->homes[$player][$home] ?? null;
         } 
     }
+    
     public function addHome(Player $player, string $home): void{
         $this->homes[$player->getName()][$home] = new Home($home);
     }
+    
     public function deleteHome(Player $player, string $home): void {
         unset($this->homes[$player->getName()][$home]);
     }
+    
     public function save(): void {
-          @mkdir(HomeSystem::getInstance()->getDataFolder() . 'players');
+        @mkdir(HomeSystem::getInstance()->getDataFolder() . 'players');
         
         foreach ($this->homes as $name => $homes) {
             foreach($homes as $home) {
-            $config = new Config(HomeSystem::getInstance()->getDataFolder() . 'players/' . $name . '.yml', Config::YAML);
-            $config->set(
-               $home->getHomeName(), 
-               $home->data());
-            $config->save();
+               $config = new Config(HomeSystem::getInstance()->getDataFolder() . 'players/' . $name . '.yml', Config::YAML);
+               $config->set(
+                     $home->getHomeName(), 
+                     $home->data()
+               );
+               $config->save();
             } 
             
-    } 
+       } 
     }
     public function load(): void {
         $files = glob(HomeSystem::getInstance()->getDataFolder() . 'players/*.yml');
@@ -60,5 +68,5 @@ class HomeFactory {
                $homeEdit->setPosition(Utils::getInstance()->stringToPosition($data["position"]));
            } 
      } 
-}
+  }
 }
